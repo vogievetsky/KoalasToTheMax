b949c9f361244a99d09f5ff08c09d7311073ee43
Vadim Ogievetsky <vadimon@gmail.com>
2012-07-14 09:28:06 -0700

    modularized

--- koala.js
+++ koala.js
@@ -18,218 +18,228 @@
 * (try to guess the image above)
 */
 
-var vis,
-    maxSize = 512,
-    minSize = 4;
+var koala = {
+  version: '1.4.0'
+};
 
-// Find the color average of 4 colors in the RGB colorspace
-function avgCol(x, y, z, w) {
-  return [
-    (x[0] + y[0] + z[0] + w[0]) / 4,
-    (x[1] + y[1] + z[1] + w[1]) / 4,
-    (x[2] + y[2] + z[2] + w[2]) / 4
-  ];
-}
+koala.supportsSVG = function() {
+  return !!document.createElementNS && !!document.createElementNS('http://www.w3.org/2000/svg', "svg").createSVGRect;
+};
 
-function colToRGB(col) {
-  return 'rgb(' + col.map(Math.round).join(',') + ')';
-}
+koala.loadImage = (function() {
+  var vis,
+      maxSize = 512,
+      minSize = 4;
 
-// Some people tend to try and click the circles.
-var numClicks = 0;
-function onCircleClick() {
-  numClicks++;
-  if (numClicks !== 5) return;
-  //alert("You don't have to click, you can just mouse over. :-)");
-}
-
-function loadImage(imageData, onEvent) {
-  onEvent = onEvent || function() {};
-  var dim  = maxSize / minSize;
-  var canvas, data;
-
-  try {
-    canvas = document.createElement('canvas').getContext('2d');
-    canvas.drawImage(imageData, 0, 0, dim, dim);
-    data = canvas.getImageData(0, 0, dim, dim).data;
-  } catch(e) {
-    alert("Failed to load image.");
-    return;
+  // Find the color average of 4 colors in the RGB colorspace
+  function avgCol(x, y, z, w) {
+    return [
+      (x[0] + y[0] + z[0] + w[0]) / 4,
+      (x[1] + y[1] + z[1] + w[1]) / 4,
+      (x[2] + y[2] + z[2] + w[2]) / 4
+    ];
   }
 
-  // Got the data now build the tree
-  var prevLayer = {};
-  var layer;
-  var size = minSize;
-  var x, y, t, col;
+  function colToRGB(col) {
+    return 'rgb(' + col.map(Math.round).join(',') + ')';
+  }
 
+  // Some people tend to try and click the circles.
+  var numClicks = 0;
+  function onCircleClick() {
+    numClicks++;
+    if (numClicks !== 5) return;
+    //alert("You don't have to click, you can just mouse over. :-)");
+  }
 
-  t = 0;
-  for (y = 0; y < dim; y++) {
-    for (x = 0; x < dim; x++) {
-      col = [data[t], data[t+1], data[t+2]];
-      prevLayer[[x,y]] = {
-        x:   size * (x + .5),
-        y:   size * (y + .5),
-        r:   size / 2,
-        col: col,
-        rgb: colToRGB(col)
-      };
-      t += 4;
+  return function(imageData, onEvent) {
+    onEvent = onEvent || function() {};
+    var dim  = maxSize / minSize;
+    var canvas, data;
+
+    try {
+      canvas = document.createElement('canvas').getContext('2d');
+      canvas.drawImage(imageData, 0, 0, dim, dim);
+      data = canvas.getImageData(0, 0, dim, dim).data;
+    } catch(e) {
+      alert("Failed to load image.");
+      return;
     }
-  }
 
-  var activeLayerNumber = 0
-  var activeLayerCount = [];
-  var activeTotalCount = 0;
+    // Got the data now build the tree
+    var prevLayer = {};
+    var layer;
+    var size = minSize;
+    var x, y, t, col;
 
-  var grid = prevLayer;
 
-  var c1, c2, c3, c4;
-  while (size < maxSize) {
-    dim /= 2;
-    size = size * 2;
-    layer = {};
+    t = 0;
     for (y = 0; y < dim; y++) {
       for (x = 0; x < dim; x++) {
-        c1 = prevLayer[[2*x  , 2*y  ]];
-        c2 = prevLayer[[2*x+1, 2*y  ]];
-        c3 = prevLayer[[2*x  , 2*y+1]];
-        c4 = prevLayer[[2*x+1, 2*y+1]];
-        col = avgCol(c1.col, c2.col, c3.col, c4.col);
-        layer[[x,y]] = c1.parent = c2.parent = c3.parent = c4.parent = {
+        col = [data[t], data[t+1], data[t+2]];
+        prevLayer[[x,y]] = {
           x:   size * (x + .5),
           y:   size * (y + .5),
           r:   size / 2,
           col: col,
-          rgb: colToRGB(col),
-          children: [c1, c2, c3, c4],
-          layer: activeLayerNumber
+          rgb: colToRGB(col)
         };
+        t += 4;
       }
     }
-    activeLayerCount.push(dim * dim);
-    activeTotalCount += dim * dim;
-    activeLayerNumber++;
-    prevLayer = layer;
-  }
 
-  // Make sure that the svg exists and is empty
-  if (!vis) {
-    // Create the SVG ellement
-    vis = d3.select("div#dots")
-      .append("svg")
-        .attr("width", maxSize)
-        .attr("height", maxSize);
-  } else {
-    vis.selectAll('circle').remove();
-  }
+    var activeLayerNumber = 0
+    var activeLayerCount = [];
+    var activeTotalCount = 0;
 
-  var nextPercent = 0;
-  function split(d) {
-    if (!d.node || !d.children) return;
-    d3.select(d.node).remove();
-    delete d.node;
-    addCircles(d.children);
+    var grid = prevLayer;
 
-    // manage events
-    var l = d.layer;
-    activeLayerCount[l]--;
-    if (activeLayerCount[l] === 0) {
-      onEvent('LayerClear', l);
+    var c1, c2, c3, c4;
+    while (size < maxSize) {
+      dim /= 2;
+      size = size * 2;
+      layer = {};
+      for (y = 0; y < dim; y++) {
+        for (x = 0; x < dim; x++) {
+          c1 = prevLayer[[2*x  , 2*y  ]];
+          c2 = prevLayer[[2*x+1, 2*y  ]];
+          c3 = prevLayer[[2*x  , 2*y+1]];
+          c4 = prevLayer[[2*x+1, 2*y+1]];
+          col = avgCol(c1.col, c2.col, c3.col, c4.col);
+          layer[[x,y]] = c1.parent = c2.parent = c3.parent = c4.parent = {
+            x:   size * (x + .5),
+            y:   size * (y + .5),
+            r:   size / 2,
+            col: col,
+            rgb: colToRGB(col),
+            children: [c1, c2, c3, c4],
+            layer: activeLayerNumber
+          };
+        }
+      }
+      activeLayerCount.push(dim * dim);
+      activeTotalCount += dim * dim;
+      activeLayerNumber++;
+      prevLayer = layer;
     }
 
-    var percent = 1 - d3.sum(activeLayerCount) / activeTotalCount;
-    if (percent >= nextPercent) {
-      onEvent('PercentClear', Math.round(nextPercent * 100));
-      nextPercent += 0.05;
+    // Make sure that the svg exists and is empty
+    if (!vis) {
+      // Create the SVG ellement
+      vis = d3.select("div#dots")
+        .append("svg")
+          .attr("width", maxSize)
+          .attr("height", maxSize);
+    } else {
+      vis.selectAll('circle').remove();
     }
-  }
 
-  function addCircles(circles, init) {
-    var circle = vis
-      .selectAll('.nope')
-        .data(circles)
-        .enter().append('circle');
+    var nextPercent = 0;
+    function split(d) {
+      if (!d.node || !d.children) return;
+      d3.select(d.node).remove();
+      delete d.node;
+      addCircles(d.children);
 
-    if (init) {
-      // Setup the initial state of the initial circle
-      circle = circle
+      // manage events
+      var l = d.layer;
+      activeLayerCount[l]--;
+      if (activeLayerCount[l] === 0) {
+        onEvent('LayerClear', l);
+      }
+
+      var percent = 1 - d3.sum(activeLayerCount) / activeTotalCount;
+      if (percent >= nextPercent) {
+        onEvent('PercentClear', Math.round(nextPercent * 100));
+        nextPercent += 0.05;
+      }
+    }
+
+    function addCircles(circles, init) {
+      var circle = vis
+        .selectAll('.nope')
+          .data(circles)
+          .enter().append('circle');
+
+      if (init) {
+        // Setup the initial state of the initial circle
+        circle = circle
+          .attr('cx',   function(d) { return d.x; })
+          .attr('cy',   function(d) { return d.y; })
+          .attr('r', 4)
+          .attr('fill', 'rgb(255,255,255)')
+            .transition()
+            .duration(1000);
+      } else {
+        // Setup the initial state of the opened circles
+        circle = circle
+          .attr('cx',   function(d) { d = d.parent; return d.x; })
+          .attr('cy',   function(d) { d = d.parent; return d.y; })
+          .attr('r',    function(d) { d = d.parent; return d.r; })
+          .attr('fill', function(d) { d = d.parent; return d.rgb; })
+          .attr('fill-opacity', 0.68)
+            .transition()
+            .duration(300);
+      }
+
+      // Transition the to the respective final state
+      circle
         .attr('cx',   function(d) { return d.x; })
         .attr('cy',   function(d) { return d.y; })
-        .attr('r', 4)
-        .attr('fill', 'rgb(255,255,255)')
-          .transition()
-          .duration(1000);
-    } else {
-      // Setup the initial state of the opened circles
-      circle = circle
-        .attr('cx',   function(d) { d = d.parent; return d.x; })
-        .attr('cy',   function(d) { d = d.parent; return d.y; })
-        .attr('r',    function(d) { d = d.parent; return d.r; })
-        .attr('fill', function(d) { d = d.parent; return d.rgb; })
-        .attr('fill-opacity', 0.68)
-          .transition()
-          .duration(300);
+        .attr('r',    function(d) { return d.r; })
+        .attr('fill', function(d) { return d.rgb; })
+        .attr('fill-opacity', 1)
+        .each('end',  function(d) {
+            d.node = this;
+            d3.select(this).attr('class', 'ready');
+          });
     }
 
-    // Transition the to the respective final state
-    circle
-      .attr('cx',   function(d) { return d.x; })
-      .attr('cy',   function(d) { return d.y; })
-      .attr('r',    function(d) { return d.r; })
-      .attr('fill', function(d) { return d.rgb; })
-      .attr('fill-opacity', 1)
-      .each('end',  function(d) {
-          d.node = this;
-          d3.select(this).attr('class', 'ready');
-        });
-  }
+    function findBase(pos) {
+      var x = Math.floor(pos[0] / minSize),
+          y = Math.floor(pos[1] / minSize),
+          base = grid[[x,y]];
+      if (!base) return null;
+      while (base && !base.node) base = base.parent;
+      return base || null;
+    }
 
-  function findBase(pos) {
-    var x = Math.floor(pos[0] / minSize),
-        y = Math.floor(pos[1] / minSize),
-        base = grid[[x,y]];
-    if (!base) return null;
-    while (base && !base.node) base = base.parent;
-    return base || null;
-  }
+    var posPrev = [];
+    function findAndSplit(posFn) {
+      return function() {
+        var pos = posFn(),
+            n = pos.length;
 
-  var posPrev = [];
-  function findAndSplit(posFn) {
-    return function() {
-      var pos = posFn(),
-          n = pos.length;
+        for (var i = 0; i < n; i++) {
+          var p = pos[i];
+          var d = findBase(p);
+          if (d && d.children) {
+            var pp  = posPrev[i] || [0,0],
+                dx  = d.x - p[0],
+                dy  = d.y - p[1],
+                dxp = d.x - pp[0],
+                dyp = d.y - pp[1],
+                r2  = d.r * d.r;
 
-      for (var i = 0; i < n; i++) {
-        var p = pos[i];
-        var d = findBase(p);
-        if (d && d.children) {
-          var pp  = posPrev[i] || [0,0],
-              dx  = d.x - p[0],
-              dy  = d.y - p[1],
-              dxp = d.x - pp[0],
-              dyp = d.y - pp[1],
-              r2  = d.r * d.r;
-
-          if (dx*dx + dy*dy <= r2 && dxp*dxp + dyp*dyp >= r2) split(d);
+            if (dx*dx + dy*dy <= r2 && dxp*dxp + dyp*dyp >= r2) split(d);
+          }
+          posPrev[i] = p;
         }
-        posPrev[i] = p;
+        d3.event.preventDefault();
       }
-      d3.event.preventDefault();
     }
-  }
 
-  function mouseFn() { return [d3.svg.mouse(vis.node())]; }
-  function touchFn() { return d3.svg.touches(vis.node()); }
+    function mouseFn() { return [d3.svg.mouse(vis.node())]; }
+    function touchFn() { return d3.svg.touches(vis.node()); }
 
-  d3.select('body')
-    .on('mousemove', findAndSplit(mouseFn))
-    .on('touchmove', findAndSplit(touchFn));
+    d3.select('body')
+      .on('mousemove', findAndSplit(mouseFn))
+      .on('touchmove', findAndSplit(touchFn));
 
-  // Create the initial circle
-  addCircles([layer[[0,0]]], true);
-}
+    // Create the initial circle
+    addCircles([layer[[0,0]]], true);
+  };
+})();
 
 
 
